66f6bac287d5675f58782a51bee7a446cee6a60a
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-05-27 16:53:09 +0530

    init git

---
+++ src/routes/Note_routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const Note = require("./../models/Note");
+
+router.post("/list", async function (req, res) {
+  var notes = await Note.find({ userid: req.body.userid });
+  res.json(notes);
+});
+
+router.post("/add", async function (req, res) {
+  await Note.deleteOne({ id: req.body.id });
+  const newNote = new Note({
+    id: req.body.id,
+    userid: req.body.userid,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  await newNote.save();
+
+  const response = { massage: "New Note Created! " };
+  res.json(response);
+});
+
+router.post("/delete", async function (req, res) {
+  await Note.deleteOne({ id: req.body.id });
+  const response = { massage: "Note Deleted! " };
+  res.json(response);
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -10,40 +10,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://nanu:nanu123@cluster0.hcu8y.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(function () {
-    //APP Routes
+const mongodbPath =
+  "mongodb+srv://nanu:nanu123@cluster0.hcu8y.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongodbPath).then(function () {
+  //APP Routes
 
-    //home rout (/)
-    app.get("/", function (req, res) {
-      res.send("this is the home pase");
-    });
-    //notes rout
-    app.get("/notes/list/:userid", async function (req, res) {
-      var notes = await Note.find({ userid: req.params.userid });
-      res.json(notes);
-    });
-
-    app.post("/notes/add", async function (req, res) {
-      //res.json(req.body);
-
-      const newNote = new Note({
-        id: req.body.id,
-        userid: req.body.userid,
-        title: req.body.title,
-        content: req.body.connect,
-      });
-      await newNote.save();
-
-      const response = { massage: "New Note Created! " };
-      res.json(response);
-    });
+  //home rout (/)
+  app.get("/", function (req, res) {
+    const response = { massage: "API work!" };
+    res.json(response);
   });
 
+  const noteRouter = require("./routes/Note_routes");
+  app.use("/notes", noteRouter);
+});
+
 //Starting the server on a PORT
-app.listen(5000, function () {
-  console.log("Server started at PORT: 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  console.log("Server started at PORT: " + PORT);
 });
